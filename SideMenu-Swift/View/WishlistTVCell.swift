e75374533efaf1833c30390ff63e6074026618aa
Arvind <arvindp@alohatechnologydev.com>
2020-11-26 14:24:07 +0530

    Added Changes

--- SideMenu-Swift/View/WishlistTVCell.swift
+++ SideMenu-Swift/View/WishlistTVCell.swift
@@ -15,9 +15,7 @@ class ProductTVCell: UITableViewCell {
     let containerView = UIView()
     let imgView = UIImageView()
     let titleLbl = UILabel()
-    let dimsBG = UIView()
-    var imgViewTopConst: NSLayoutConstraint!
-    
+
     var product: Product! {
         didSet {
             titleLbl.text = product.productDiscription
@@ -69,21 +67,14 @@ extension ProductTVCell {
         //TODO: - ImgageView
         imgView.contentMode = .scaleAspectFill
         imgView.clipsToBounds = true
-        imgView.image = UIImage(named: "accessories")
+        imgView.image = UIImage(named: "nike-shoes-two")
         containerView.addSubview(imgView)
         imgView.translatesAutoresizingMaskIntoConstraints = false
-        
-        //TODO: - BGView
-        dimsBG.backgroundColor = UIColor(hex: 0x000000, alpha: 0.8)
-        dimsBG.clipsToBounds = true
-        dimsBG.layer.cornerRadius = 8.0
-        containerView.insertSubview(dimsBG, aboveSubview: imgView)
-        dimsBG.translatesAutoresizingMaskIntoConstraints = false
-        
+     
         //TODO: - TitleLbl
         titleLbl.configureNameForCell(false, txtColor: .white, fontSize: 17.0, isTxt: "CATEGORIES", fontN: fontNamedBold)
         titleLbl.textAlignment = .center
-        containerView.insertSubview(titleLbl, aboveSubview: dimsBG)
+        containerView.insertSubview(titleLbl, aboveSubview: imgView)
         titleLbl.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -93,49 +84,16 @@ extension ProductTVCell {
             containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5.0),
         ])
         
-        imgViewTopConst = imgView.topAnchor.constraint(equalTo: containerView.topAnchor)
         NSLayoutConstraint.activate([
-            imgViewTopConst,
+            imgView.topAnchor.constraint(equalTo: containerView.topAnchor),
             imgView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
             imgView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
             imgView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
             
-            dimsBG.topAnchor.constraint(equalTo: containerView.topAnchor),
-            dimsBG.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-            dimsBG.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-            dimsBG.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
-            
             titleLbl.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            titleLbl.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            titleLbl.centerYAnchor.constraint(equalTo: containerView.centerYAnchor, constant: 40),
             titleLbl.leadingAnchor.constraint(lessThanOrEqualTo: containerView.leadingAnchor, constant: 5.0),
             titleLbl.trailingAnchor.constraint(lessThanOrEqualTo: containerView.trailingAnchor, constant: 5.0),
         ])
-        
-        setupDarkMode()
-    }
-}
-
-//MARK: - DarkMode
-
-extension ProductTVCell {
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        setupDarkMode()
-    }
-    
-    private func setupDarkMode() {
-        if #available(iOS 12.0, *) {
-            switch traitCollection.userInterfaceStyle {
-            case .light, .unspecified: setupDarkModeView()
-            case .dark: setupDarkModeView(true)
-            default: break
-            }
-        } else {
-            setupDarkModeView()
-        }
-    }
-    
-    private func setupDarkModeView(_ isDarkMode: Bool = false) {
-        dimsBG.backgroundColor = UIColor(hex: 0x000000, alpha: isDarkMode ? 0.4 : 0.7)
     }
 }
